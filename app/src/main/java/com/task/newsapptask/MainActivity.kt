c3fe89fd958e5a3cb9226547520d7b05db39468c
AlaaMeaad <3la2.me3ad@gmail.com>
2021-08-06 21:28:47 +0200

    NightMode

--- app/src/main/java/com/task/newsapptask/MainActivity.kt
+++ app/src/main/java/com/task/newsapptask/MainActivity.kt
@@ -2,6 +2,7 @@ package com.task.newsapptask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
     }
 }
